45cbc43497c068ec3a4a7058bf3a7b5a5331547a
fariborzvrm <fariborz.vrm@gmail.com>
2026-06-25 12:50:25 +0330

    Add project files.

---
+++ TaskManager.API/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.API.Models;
+
+namespace TaskManager.API
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("\"Server=(localdb)\\\\mssqllocaldb;Database=TaskApiDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Models.TaskItem> TaskItems {  get; set; }
+        
+    }
+}
---
+++ TaskManager.API/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace TaskManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/task")]
+    public class TasksController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllTasks()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks); 
+        }
+    }
+}
